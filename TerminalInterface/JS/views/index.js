5e0d92b985ad89900b7ce48a9112fda70a7c0c90
Haldgerd <haldgerd.code@gmail.com>
2021-12-03 20:17:26 +0100

    "ERROR: fetching image from external API, animations added, design finished."

--- TerminalInterface/JS/views/index.js
+++ TerminalInterface/JS/views/index.js
@@ -3,6 +3,7 @@ const h1 = document.getElementById("title");
 const setShadow = () => {
 
   let displace = 0.2 + Math.random() * 1.7;
+ 
 
   const shadow = `${displace}px 0px 1px rgba(0, 70, 255, 0.6), ${-displace}px 0px 1px rgba(255, 50, 0, 0.6), 0 0 4px`;
 
